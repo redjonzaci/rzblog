6e3f94579006d153809fb4ba447c190d1a25da93
Redjon Zaci <redjonzaci@gmail.com>
2020-12-11 21:00:04 +0100

    Registered models of blog app to admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Blogger, Blog, BlogComment
 
-# Register your models here.
+admin.site.register(Blogger)
+# admin.site.register(Blog)
+admin.site.register(BlogComment)
+
+class BlogCommentsInline(admin.TabularInline):
+    model = BlogComment
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    inlines = [BlogCommentsInline]
