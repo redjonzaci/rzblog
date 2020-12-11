d2e40802dfbdc65471eb399967dbb8ddece4e644
Redjon Zaci <redjonzaci@gmail.com>
2020-12-11 21:29:50 +0100

    Set up home page of blog app

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Blogger, Blog, BlogComment
+from django.contrib.auth.models import User
 
-# Create your views here.
+def index(request):
+    """View function for home page of site."""
+
+    num_bloggers = Blogger.objects.all().count()
+    num_blogs = Blog.objects.all().count()   
+    total_comments = BlogComment.objects.all().count()
+    
+    context = {
+        'num_bloggers': num_bloggers,
+        'num_blogs': num_blogs,
+        'total_comments': total_comments,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
