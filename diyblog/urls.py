49040654e6b825b901fc9a03fdd2aa3c6df77320
Redjon Zaci <redjonzaci@gmail.com>
2020-12-11 01:18:31 +0100

    Included blog app URLs to project URLs

--- diyblog/urls.py
+++ diyblog/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+# Used include() to add paths from the blog app
+from django.urls import path, include
+# Redirected the base URL to the blog app
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
+    path('', RedirectView.as_view(url='blog/', permanent=True)),
 ]
